12af9150c84327937c69881fc044b455850db5a8
kamal <kamalkj9368@gmail.com>
2023-07-24 18:20:52 +0530

    second commit

--- ZomatoClone/src/UI/Filter.jsx
+++ ZomatoClone/src/UI/Filter.jsx
@@ -1,18 +1,14 @@
 import { useState } from "react"
-import Modal from "./Modal"
+import { Link } from "react-router-dom"
+
 const Filter = (props) => {
-  const [showModal,setShowModal] = useState(false)
-  const onClickEventHandler = () =>{
-    setShowModal(true)
-    props.showModalFunction(showModal)
-  }
   return (
     <>
-    <button onClick={onClickEventHandler} className="col  border-0">
+    <Link className="col border-2 ">
     <div className="icon-link icon-link-hover text-decoration-none col justify-start" style={{ '--bs-icon-link-transform': 'translate3d(0, -.125rem, 0)' }} href="#">
             {props.svg} {props.name}
 </div>
-    </button>
+    </Link>
     </>
   )
 }
